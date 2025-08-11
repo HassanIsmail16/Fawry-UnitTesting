902e9d190739bc931218baab7a934fd2ac2f0e28
Hassan Ali <162273873+HassanIsmail16@users.noreply.github.com>
2025-08-12 01:28:33 +0300

    added insufficient balance & credict exceeded test

--- src/test/java/example/account/AccountManagerTest.java
+++ src/test/java/example/account/AccountManagerTest.java
@@ -58,4 +58,20 @@ public class AccountManagerTest {
         Assertions.assertThat(result).isEqualTo("success");
         Assertions.assertThat(c.getBalance()).isEqualTo(-100);
     }
+
+    @Test
+    void givenCustomerWithInsufficientBalanceAndCreditLimitExceeded_whenWithdraw_thenSucceed() {
+        // Arrange
+        AccountManager am = new AccountManagerImpl();
+        Customer c = new Customer();
+        c.setBalance(100);
+        c.setCreditAllowed(true);
+
+        // Act
+        String result = am.withdraw(c, 2000);
+
+        // Assert
+        Assertions.assertThat(result).isEqualTo("maximum credit exceeded");
+        Assertions.assertThat(c.getBalance()).isEqualTo(100);
+    }
 }
